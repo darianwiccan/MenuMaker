15c151f359893441d2e40308bacc37038ccce705
jsthomas45320 <thomas.611@wright.edu>
2015-12-18 08:25:58 -0500

    Database Statement class added.

---
+++ MenuMaker/src/database/DatabaseStatement.java
@@ -0,0 +1,37 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DatabaseStatement {
+
+	public static ResultSet executeQuery(Connection conn, String command) throws SQLException {
+	    Statement stmt = null;
+	    ResultSet rs = null;
+	    try {
+	        stmt = conn.createStatement();
+	        rs = stmt.executeQuery(command); // This will throw a SQLException if it fails
+	        return rs;
+	    } catch (SQLException e) {
+	    	
+	    }
+        return rs;
+	}
+	
+	public static boolean executeUpdate(Connection conn, String command) throws SQLException {
+	    Statement stmt = null;
+	    try {
+	        stmt = conn.createStatement();
+	        stmt.executeUpdate(command); // This will throw a SQLException if it fails
+	        return true;
+	    } finally {
+
+	    	// This will run whether we throw an exception or not
+	    	stmt.close();
+//	        if (stmt != null) { stmt.close(); }
+	    }
+	}
+}
