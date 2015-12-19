0af521765782bb40882f080163ad43cb98c200e3
jsthomas45320 <thomas.611@wright.edu>
2015-12-19 10:30:42 -0500

    Added MenuMakerGUI class.

---
+++ MenuMaker/src/gui/MenuMakerGUI.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class MenuMakerGUI {
+	
+	public static final JFrame myFrame = new JFrame();
+
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	public static void buildGui() {
+		myFrame.setBounds(100, 100, 400, 300);
+	}
+	
+}
