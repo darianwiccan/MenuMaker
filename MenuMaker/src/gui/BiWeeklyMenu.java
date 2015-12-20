ac8e8982422465a7003ca971964d9b883eed468b
jsthomas45320 <thomas.611@wright.edu>
2015-12-20 07:12:27 -0500

    Added functionality to BiWeeklyMenu class.

--- MenuMaker/src/gui/BiWeeklyMenu.java
+++ MenuMaker/src/gui/BiWeeklyMenu.java
@@ -127,17 +127,189 @@ public class BiWeeklyMenu {
 			txtSpecialMealDesc[i].setText("* Special Event Description");
 			txtSpecialMealDesc[i].setVisible(false);
 			panel.add(txtSpecialMealDesc[i]);
+			
+			cboDayOption[i].addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					switch (cboDayOption[j].getSelectedIndex()) {
+					case 0:
+						break;
+					case 1:
+						break;
+					case 2:
+						lblDailyMeal[j].setVisible(false);
+						txtDailyMeal[j].setVisible(true);
+						cboMealChoice[j].setVisible(false);
+						txtSpecialMealName[j].setVisible(false);
+						txtSpecialMealDesc[j].setVisible(false);
+						break;
+					case 3:
+						lblDailyMeal[j].setVisible(false);
+						txtDailyMeal[j].setVisible(false);
+						cboMealChoice[j].setVisible(true);
+						cboMealChoice[j].setEditable(true);
+						txtSpecialMealName[j].setVisible(false);
+						txtSpecialMealDesc[j].setVisible(false);
+						cboMealChoice[j].setSelectedItem("Please select a meal");
+						break;
+					case 4:
+						cboCategoryChoice[j].setVisible(true);
+						cboMealChoice[j].setVisible(false);
+						txtDailyMeal[j].setVisible(false);
+						lblDailyMeal[j].setVisible(false);
+						txtSpecialMealName[j].setVisible(false);
+						txtSpecialMealDesc[j].setVisible(false);
+						break;
+					case 5:
+						cboCategoryChoice[j].setVisible(false);
+						cboMealChoice[j].setVisible(false);
+						txtDailyMeal[j].setVisible(false);
+						lblDailyMeal[j].setVisible(false);
+						txtSpecialMealName[j].setVisible(false);
+						txtSpecialMealDesc[j].setVisible(false);
+						break;
+					case 6:
+						cboCategoryChoice[j].setVisible(false);
+						cboMealChoice[j].setVisible(false);
+						txtDailyMeal[j].setVisible(false);
+						lblDailyMeal[j].setVisible(false);
+						txtSpecialMealName[j].setVisible(true);
+						txtSpecialMealDesc[j].setVisible(true);
+						break;
+					}					
+				}
+			});
 		}
 			
 		JButton btnGenerateMenu = new JButton("Generate");
+		btnGenerateMenu.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				vecSelectedOptions = fillOptionVec();
+				try {
+					generateMenu(vecSelectedOptions);
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+		});
 		
 		JButton btnSaveMenu = new JButton("Save");
+		btnSaveMenu.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Boolean updateSuccess = null;
+				try {
+					DatabaseStatement.executeUpdate(conn, "delete from currentmenu");
+					if (menuList.size() < 14) {
+						JOptionPane.showMessageDialog(null, "Menu not completed!");
+					} else {
+						for (int i = 0; i < menuList.size(); i++) {
+							if (menuList.get(i) == null) {
+								if (cboDayOption[i].getSelectedIndex() == 1) {
+									System.out.println("menuList.get(" + i + ") is NULL.");
+									updateSuccess = DatabaseStatement.executeUpdate(conn, "insert into currentmenu values(" + 
+											i + ", 'No Meal', 'Out of Town', " + -1 + ")");
+								} else if (cboDayOption[i].getSelectedIndex() == 5) {
+									System.out.println("menuList.get(" + i + ") is NULL.");
+									updateSuccess = DatabaseStatement.executeUpdate(conn, "insert into currentmenu values(" + 
+											i + ", 'No Meal', 'Shopping Day - Take Out', " + -1 + ")");
+								}
+							} else {
+								rs = DatabaseStatement.executeQuery(conn, "select recid, recname, recdesc from "
+										+ "recipes where recid = " + menuList.get(i).getRecID());
+								if (rs == null) {
+									System.out.println("Item not found in database. Unable to add to currentmenu.");
+									updateSuccess = false;
+								} else {
+									rs = DatabaseStatement.executeQuery(conn, "select recid, recname, recdesc from "
+											+ "recipes where recid = " + menuList.get(i).getRecID());
+									updateSuccess = DatabaseStatement.executeUpdate(conn, "insert into currentmenu values( " + 
+											i + ", '" + menuList.get(i).getRecName() + "', '" + menuList.get(i).getRecDesc() + 
+											"', " + menuList.get(i).getRecID() + ")");
+								}
+							}
+							if (updateSuccess == false) {
+								JOptionPane.showMessageDialog(null, "Table not updated!!!");								
+							}
+						}
+					}
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+			
+		});
 		
 		JButton btnLoadCurMenu = new JButton("Load Current");
 		
 		JButton btnClear = new JButton("Clear Menu");
 		
 		JButton btnSaveToFile = new JButton("Save to File");
+		btnSaveToFile.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String dailyString;
+				try {
+					PrintWriter writer = new PrintWriter("menu.txt");
+					
+					for (int i = 0; i < 14; i++) {
+						switch (cboDayOption[i].getSelectedIndex()) {
+						case 0:
+							dailyString = lblDay[i].getText();
+							dailyString += "   ";
+							dailyString += lblDailyMeal[i].getText();
+							writer.println(dailyString);
+							break;
+						case 1:
+							dailyString = lblDay[i].getText();
+							dailyString += "   ";
+							dailyString += "No Meal - Out of Town";
+							writer.println(dailyString);
+							break;
+						case 2:
+							dailyString = lblDay[i].getText();
+							dailyString += "   ";
+							dailyString += lblDailyMeal[i].getText();
+							writer.println(dailyString);
+							break;
+						case 3:
+							dailyString = lblDay[i].getText();
+							dailyString += "   ";
+							dailyString += lblDailyMeal[i].getText();
+							writer.println(dailyString);
+							break;
+						case 4:
+	 						dailyString = lblDay[i].getText();
+							dailyString += "   ";
+							dailyString += lblDailyMeal[i].getText();
+							writer.println(dailyString);
+							break;
+						case 5:
+							dailyString = lblDay[i].getText();
+							dailyString += "   ";
+							dailyString += "Take Out - Shopping Day";
+							writer.println(dailyString);
+							break;
+						case 6:
+							dailyString = lblDay[i].getText();
+							dailyString += "   ";
+							dailyString += lblDailyMeal[i].getText();
+							writer.println(dailyString);
+							break;
+						} // END switch (cboDayOption[i].getSelectedIndex())
+					} // END for (int i = 0; i < 14; i++)
+					writer.close();
+				} catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
 		
 		JButton btnClose = new JButton("Close Window");
 		btnClose.addActionListener(new ActionListener() {
@@ -168,6 +340,18 @@ public class BiWeeklyMenu {
 		myFrame.setVisible(true);
 		panel.setVisible(true);
 		cont.setVisible(true);
+	}
+	
+	public static void generateMenu(Vector<Integer> vecIntVec) throws SQLException {
+		
+	}
+	
+	public static Vector<Integer> fillOptionVec() {
+		Vector<Integer> tempVec = new Vector<Integer>();
+		for (int i = 0; i < 14; i++) {
+			tempVec.add(i, cboDayOption[i].getSelectedIndex());
+		}
+		return tempVec;
 	}
 	
 	private static String[] loadCategories(Connection conn) throws SQLException {
